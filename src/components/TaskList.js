5a0a4ac1e94099376de50cc728992fc434969429
spaenleh <basile.1358@gmail.com>
2022-08-01 14:03:16 +0200

    feat: add tasklist

---
+++ src/components/TaskList.js
@@ -0,0 +1,70 @@
+import React from "react";
+
+import Task from "./Task";
+import PropTypes from "prop-types";
+
+export default function TaskList({ loading, tasks, onPinTask, onArchiveTask }) {
+  const events = {
+    onPinTask,
+    onArchiveTask,
+  };
+  const LoadingRow = (
+    <div className="loading-item">
+      <span className="glow-checkbox" />
+      <span className="glow-text">
+        <span>Loading</span> <span>cool</span> <span>text</span>
+      </span>
+    </div>
+  );
+
+  if (loading) {
+    return (
+      <div className="list-items" data-testid={"loading"} key={"loading"}>
+        {LoadingRow}
+        {LoadingRow}
+        {LoadingRow}
+        {LoadingRow}
+        {LoadingRow}
+        {LoadingRow}
+      </div>
+    );
+  }
+
+  if (tasks.length === 0) {
+    return (
+      <div className="list-items" data-testid="empty" key="empty">
+        <div className={"wrapper-message"}>
+          <span className="icon-check" />
+          <p className="title-message">You have no tasks</p>
+          <p className="subtitle-message">Sit back and relax</p>
+        </div>
+      </div>
+    );
+  }
+
+  const tasksInOrder = [
+    ...tasks.filter((t) => t.state === "TASK_PINNED"),
+    ...tasks.filter((t) => t.state !== "TASK_PINNED"),
+  ];
+  return (
+    <div className={"list-items"}>
+      {tasksInOrder.map((task) => (
+        <Task key={task.id} task={task} {...events} />
+      ))}
+    </div>
+  );
+}
+
+TaskList.propTypes = {
+  /** Checks if it's in loading state */
+  loading: PropTypes.bool,
+  /** The list of taks */
+  tasks: PropTypes.arrayOf(Task.propTypes.task).isRequired,
+  /** Event to change the task to pinned */
+  onPinTask: PropTypes.func,
+  /** Event to change task to archived */
+  onArchiveTask: PropTypes.func,
+};
+TaskList.defaultProps = {
+  loading: false,
+};
